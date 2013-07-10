177de5af4532b20a4ec513858341eb64916878cf
Ubuntu <ubuntu@ip-10-154-147-90.ec2.internal>
2013-07-10 18:03:17 +0000

    achanging 5

--- web.js
+++ web.js
@@ -6,16 +6,14 @@ fs.readFile('index.html', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
-  console.log(data);
+    var buffer = new Buffer(data, "utf-8")
 });
 
 app.get('/', function(request, response) {
-  response.send(data);
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-
