ce3916f9dfe9ca84152bf3296e76fb3cb98d675b
Ubuntu <ubuntu@ip-10-154-147-90.ec2.internal>
2013-07-10 17:53:56 +0000

    achanging l

--- web.js
+++ web.js
@@ -1,12 +1,21 @@
+fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
+fs.readFile('index.html', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
+});
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
